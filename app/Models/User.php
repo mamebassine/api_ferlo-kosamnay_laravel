e04aa8f7b5d96ce28027f54d94c0c1e85ca7ac21
mamebassine <mamebassine06@gmail.com>
2024-09-21 15:21:29 +0000

    Configuration du model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,9 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -49,23 +50,55 @@ class User extends Authenticatable
     }
 
     // Relations
+
+    /**
+     * Relation avec les commandes de l'utilisateur.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function commandes()
     {
         return $this->hasMany(Commande::class); // Relation avec les commandes
     }
 
+    /**
+     * Relation avec la région de l'utilisateur.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function region()
     {
         return $this->belongsTo(Region::class); // Relation avec la région
     }
 
+    /**
+     * Relation avec les notifications de l'utilisateur.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function notifications()
     {
         return $this->hasMany(Notification::class); // Relation avec les notifications
     }
 
+    /**
+     * Relation avec les produits de l'utilisateur.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function produits()
     {
         return $this->hasMany(Produit::class); // Relation avec les produits
+    }
+
+    // Méthodes JWT
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(): array
+    {
+        return [];
     }
 }
